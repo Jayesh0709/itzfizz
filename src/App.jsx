41d11978f5433f71a34405173e378ba4c5459ef7
Jayesh0709 <gehlotjayesh65@gmail.com>
2026-03-01 17:29:39 +0530

    add logo and title

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,18 @@ function App() {
             <p className='font-semibold text-xl'>Increase in pick up point use</p>
           </div>
         </div>
+
+
         <div className='sticky flex top:20 items-center bg-white px-10 translate-y-15'>
 
-          <div className='h-40 w-[90%] mt-20 bg-[#45DB7D] rounded-full absolute text-center text-9xl font-stretch-50% font-bold items-center justify-center flex'><h1>WEL COME ITZFIZZ</h1></div>
+          <div className='h-40 w-[90%] mt-20 bg-[#45DB7D] rounded-full absolute text-center text-8xl font-stretch-50% font-bold items-center justify-center flex'><h1>WEL COME ITZFIZZ</h1></div>
           <div ref={circle} className='h-40 w-[90%] mt-20 bg-blue-300 rounded-full absolute'>
             <img src="https://paraschaturvedi.github.io/car-scroll-animation/McLaren%20720S%202022%20top%20view.png" alt="" className='h-40' />
           </div>
         </div>
+
+
+
         <div className='flex gap-26 w-auto justify-center translate-y-50 translate-x-30'>
           <div className='card bg-white h-50 w-50 rounded flex flex-col justify-center p-5 gap-3'>
             <h1 className='font-bold text-5xl'>23%</h1>
